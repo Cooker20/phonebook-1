768437b7d305d0077e08bd40d33ff8c9036c4043
harun <haksakal20@gmail.com>
2024-12-25 13:14:04 +0300

    files added

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "Cooker20"
+}
+
+variable "git-token" {
+  default = "**************"
+}
+
+variable "key-name" {
+  default = "firstkey"
+}
+
+variable "hosted-zone" {
+  default = "devopsharun.com.tr"
+}
